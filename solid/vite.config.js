5862a9503d045c0e8d9df905a0f7684eeecb9b30
Milan Hauth <milahu@gmail.com>
2022-10-24 15:45:58 +0200

    fix octokit

--- solid/vite.config.js
+++ solid/vite.config.js
@@ -9,4 +9,12 @@ export default defineConfig({
   build: {
     target: 'esnext',
   },
+  resolve: {
+    alias: {
+      // fix: ReferenceError: global is not defined @ node-fetch: Body.Promise = global.Promise;
+      // fix: TypeError: Cannot read properties of undefined (reading 'prototype') @ node-fetch: Stream.Readable.prototype
+      // https://github.com/octokit/octokit.js/issues/2126
+      'node-fetch': 'isomorphic-fetch',
+    },
+  },
 });
